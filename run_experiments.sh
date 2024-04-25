b70d902ebe04ec05eda3295048e4c028b102a44d
thehunk1206 <khantauhid313@gmail.com>
2024-04-25 14:29:51 +0530

    ADDED: shell script for running multiple experiments

---
+++ run_experiments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the arguments for each experiment
+declare -a experiments=(
+    "--rank 0 --mlflow_run_name exp1"
+    "--enable_galore --rank 2 --mlflow_run_name exp3"
+    "--enable_galore --rank 4 --mlflow_run_name exp4"
+    "--enable_galore --rank 8 --mlflow_run_name exp5"
+    "--enable_galore --rank 16 --mlflow_run_name exp6"
+    "--enable_galore --rank 32 --mlflow_run_name exp7"
+    "--enable_galore --rank 64 --mlflow_run_name exp8"
+    "--enable_galore --rank 128--mlflow_run_name exp2"
+
+)
+
+# Loop through each experiment and run the script
+for exp_args in "${experiments[@]}"; do
+    python train_cifar.py --mlflow_experiment_name galore_resnet --epochs 10 $exp_args
+done
